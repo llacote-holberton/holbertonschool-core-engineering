4a81ff6825d8153ae279142fb88b09905a02f215
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-04 20:46:18 +0200

    Feat: F&M T6 - Bringing the calculation

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import calculator_1
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} + {b} = {div(a, b)}")
+    # .format(
