59a38fd1c102d57a4df728cf25a9cdd5b2066a62
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-04 18:57:23 +0200

    Fix: restricting process to actual lowercase alphabet
    Indeed if the transformation was applied to any character
      it would lead to surprising results.

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 def uppercase(str):
     for c in str:
-        print(chr(ord(c)-32), end='')
+        n = ord(c)
+        if (n >= 97 and n < 97+26):
+            c = chr(ord(c)-32)
+        print(c, end='')
     print('')
 # .format()
