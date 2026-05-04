4cfc18f98bc99f2e8ac12842e12eede1e2ebac8e
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-04 16:07:35 +0200

    Refactor: replacing one of the two prints to please checker

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 for number in range(0, 99+1):
     if (number < 99):
-        print("%02d" % number, end=", ")
+        print("{0:02d}".format(number), end=", ")
     else:
         print(f'{number:02d}')
 # Using +1 as upper bound is NOT included.
