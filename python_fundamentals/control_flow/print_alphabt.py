226d5cff424b46b2f8b450680d9a67a3ca403e9a
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-04 15:27:32 +0200

    Fix: T2 removing ternary logic for 'end'
    Only way I see to please the checker which expects
      exactly 24 characters for output.

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -2,4 +2,4 @@
 for char in range(ord('a'), ord('z') + 1):
     if chr(char) not in "eq":
         # Ternary expression is inverted in Python BECAUSE WHY NOT FFS
-        print("{}".format(chr(char)), end='\n' if char == ord('z') else '')
+        print("{}".format(chr(char)), end='')
