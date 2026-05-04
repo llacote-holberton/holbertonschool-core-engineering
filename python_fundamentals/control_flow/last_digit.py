2f1df4627faab22a996f034dd5657d8a768fa91e
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-04 14:28:29 +0200

    Fix: T1 not keeping sign for negative number's last digit
    NOTE: Python likes to make very weird design choices,
      using the divisor as reference for the sign...

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -2,6 +2,8 @@
 number = __import__('random').randint(-10000, 10000)
 
 last_digit = number % 10
+if (number < 0):
+    last_digit -= 10
 msg_common_part = "Last digit of {} is {}".format(number, last_digit)
 
 if (last_digit > 5):
