74490bcf79da0be0f760c82486a5fbc8d077cce9
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-04 14:35:15 +0200

    Feat: T2 "Alphabet Game (Lowercase)"

---
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Sadly crazy weird syntax for FOR in Python
+# Ord(x) returns Unicode of a number
+# Chr(n) returns the character for a Unicode
+# BUT there is a much simpler way...
+for char in "abcdfghijklmnoprstuvwxyz":
+    print(char, end='')
+print('')
