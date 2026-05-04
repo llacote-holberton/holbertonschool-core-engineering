1914f66f1500ad0e7d01fbb2f23513a73fa1a19a
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-04 14:39:07 +0200

    Docs: removing all comments for T2
    Not sure why checker shouted here though.

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-# Ord(x) returns Unicode of a number
-# Chr(n) returns the character for a Unicode
-# BUT there is a much simpler way...
 for char in "abcdfghijklmnoprstuvwxyz":
     print(char, end='')
 print('')
