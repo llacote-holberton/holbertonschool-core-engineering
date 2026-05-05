9f49c4098317c7e5de3941b1e0949144eedaab54
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-05 10:10:10 +0200

    Feat: CDS T0 - Printing list of integers

---
+++ python_fundamentals/core_data_structures/0_main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+print_list_integer = __import__('print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
+my_list = [9, 666, -245, 890]
+print_list_integer(my_list)
