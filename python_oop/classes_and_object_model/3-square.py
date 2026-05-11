2b64649d285b53cf9d1ce00140f714e51e5d3428
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-11 15:55:55 +0200

    Py - ClasObjMod - Feat: T4 Adding area method

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module defining a Square class"""
+
+
+class Square:
+    """Square class with parameterized size
+       and basic maths methods like area"""
+    def __init__(self, size=0):
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if not (size >= 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        side = self.__size
+        return side * side
