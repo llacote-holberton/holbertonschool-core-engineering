75da0ce0cd7c71fe758cd2609e04dac4af6d187a
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-11 16:20:00 +0200

    Py - ClasObjMod - Feat: T6 Adding a visual print method

---
+++ python_oop/classes_and_object_model/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Module defining a Square class"""
+
+
+class Square:
+    """Square class with parameterized size
+       and basic maths methods like area"""
+    def __init__(self, size=0):
+        # NOT self.size() because defined as a property.
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if not (size >= 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        side = self.__size
+        return side * side
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(0, self.size):
+                for j in range(0, self.size):
+                    print('#', end="")
+                print("")
