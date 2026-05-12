523bce5430c5172aa11c6ca514cbf61cffb6ed59
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-12 10:08:02 +0200

    Py - In&Pol - T0: exploiting polymorphism
    (Step 5 of T0).

--- python_oop/inheritance/0-polymorphism_demo.py
+++ python_oop/inheritance/0-polymorphism_demo.py
@@ -15,9 +15,16 @@ class Cat(Animal):
     def speak(self):
         return "Meow"
 
-
+# Step 0 to 4
 dog = Dog()
 cat = Cat()
 
 print(dog.speak())
 print(cat.speak())
+
+# Step 5
+print("---")
+animals = [Dog(), Cat(), Dog()]
+
+for animal in animals:
+    print(animal.speak())
