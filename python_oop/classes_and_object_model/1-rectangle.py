2543a59ea7767f3054ebfe81d676989e95b40154
Laurent Lacôte <laurent.lacote@gmail.com>
2026-05-12 10:19:37 +0200

    Py - ClasObjMod - Fix: T8 uncommitted docblocs

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -5,29 +5,36 @@
 class Rectangle:
     """Rectangle class with parameterized size
        and basic maths methods like area and perimeter"""
+
     def __init__(self, width=0, height=0):
+        """"Initialize a new Rectangle instance."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Retrieve the width of the Rectangle."""
         return self.__width
 
     @property
     def height(self):
+        """Retrieve the height of the Rectangle."""
         return self.__height
 
     @width.setter
     def width(self, value):
+        """Set the width of the Rectangle with validation."""
         if (self.check_valid_dimension(value, "width")):
             self.__width = value
 
     @height.setter
     def height(self, value):
+        """Set the height of the Rectangle with validation."""
         if (self.check_valid_dimension(value, "height")):
             self.__height = value
 
     def check_valid_dimension(self, value, name="dimension"):
+        """Validate if a dimension is a positive integer."""
         if not (isinstance(value, int)):
             raise TypeError(f"{name} must be an integer")
             return False
