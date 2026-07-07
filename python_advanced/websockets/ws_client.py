34ae7311ec0dc1e0e2dde45ba2ffdf21333ab89d
Laurent Lacôte <laurent.lacote@private.com>
2026-07-07 14:03:02 +0200

    Py - WebSocks - Fix: T1 checker expected specific func
    To hold all the logic of connecting to a websocket
      and send message, both being dynamic (parameters)
      AND return the 'echo' for function caller to
      do whatever it wants with it.
    
    (NOTE: could have been nice to be clear from the get go
      on the expectation in the task's instructions...)

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -7,25 +7,32 @@ import asyncio
 from websockets.asyncio.client import connect
 
 
+async def connect_and_send(ws_server_url: str, msg_to_send: str):
+    """Tries to send a given message to target websocket uri"""
+    try:
+        async with connect(ws_server_url) as websocket:
+            await websocket.send(msg_to_send)
+            received_echo = await websocket.recv()
+            return received_echo
+    except OSError as e:
+        pass  # Connection couldn't be established.
+    except ConnectionClosed as e:
+        pass  # Connection was closed while we were discussing.
+
+
 # Connect is a corouting function => needs async main
 async def main():
     """Server 'orchestrator', setups config and start server"""
+
     protocol = 'ws://'
     host = 'localhost'
     port = 8765
     ws_server_url = protocol + host + ':' + str(port)
 
     msg_to_send = "Hello WebSocket"
-    try:
-        async with connect(ws_server_url) as websocket:
-            await websocket.send(msg_to_send)
-            received_echo = await websocket.recv()
-            print(received_echo)
-    except OSError as e:
-        pass  # Connection couldn't be established.
-    except ConnectionClosed as e:
-        pass  # Connection was closed while we were discussing.
 
+    echo_received = await connect_and_send(ws_server_url, msg_to_send)
+    print(echo_received)
 
 if __name__ == "__main__":
     # Remember: async function require being runned from
