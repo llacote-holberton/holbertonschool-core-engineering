30f4c6567f22bf997c5ca1c8b559eb0b05bb59f6
Laurent Lacôte <laurent.lacote@private.com>
2026-07-07 14:00:00 +0200

    Py - WebSocks - Feat: T1 adding simple client to try the "echo"

---
+++ python_advanced/websockets/ws_client.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Simple websocket client."""
+
+# Required to handle asynchronous tasks (imperative for websockets library)
+import asyncio
+# Using the provided "websockets-based connect" from websockets library.
+from websockets.asyncio.client import connect
+
+
+# Connect is a corouting function => needs async main
+async def main():
+    """Server 'orchestrator', setups config and start server"""
+    protocol = 'ws://'
+    host = 'localhost'
+    port = 8765
+    ws_server_url = protocol + host + ':' + str(port)
+
+    msg_to_send = "Hello WebSocket"
+    try:
+        async with connect(ws_server_url) as websocket:
+            await websocket.send(msg_to_send)
+            received_echo = await websocket.recv()
+            print(received_echo)
+    except OSError as e:
+        pass  # Connection couldn't be established.
+    except ConnectionClosed as e:
+        pass  # Connection was closed while we were discussing.
+
+
+if __name__ == "__main__":
+    # Remember: async function require being runned from
+    #   library provided function.
+    asyncio.run(main())
+
+# ========== INSTRUCTIONS =========
+# Implement a WebSocket client which will...
+# * Send exactly this message "Hello WebSocket" to a server ws://localhost:8765
+# * Wait for the server's response and print it
+#   (sent message and received response should be exactly the same string).
+# * Closes the connection afterwards.
+
+# ========== BRAINSTORM ==========
